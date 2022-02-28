d35186ec9ceee93e97f179916aad20ef8441d0e1
wwwestin <westin.h@gmail.com>
2022-02-28 14:32:55 -0500

    Completed lab

---
+++ src/components/GifListContainer.js
@@ -0,0 +1,29 @@
+import {useState, useEffect} from "react";
+import GifList from "./GifList";
+import GifSearch from "./GifSearch";
+
+function GifListContainer () {
+
+    const [gifs, setGifs] = useState([]);
+    const [query, setQuery] = useState("")
+
+    useEffect(() => {
+        fetch(
+          `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g&limit=3`
+        )
+          .then((r) => r.json())
+          .then(({ data }) => {
+            const gifs = data.map((gif) => ({ url: gif.images.original.url }));
+            setGifs(gifs);
+          });
+      }, [query]);
+
+    return (
+            <div style = {{ display: "flex" }}>
+                <GifList gifs = {gifs} />
+                <GifSearch onSubmitQuery = {setQuery} />
+            </div>
+    )
+}
+
+export default GifListContainer;
